4de11c1154a1d9a4ca9faad41d2d85c79efc47f8
Travelintrips <divatranssoetta@gmail.com>
2025-04-30 15:45:45 +0700

    Update VehicleBooking.tsx

--- src/components/booking/VehicleBooking.tsx
+++ src/components/booking/VehicleBooking.tsx
@@ -325,6 +325,30 @@ const VehicleBooking = () => {
 
   return (
     <div className="container mx-auto px-4 py-8 bg-gray-50">
+      {/*  <div className="mb-4">
+           <Button
+          variant="outline"
+          onClick={handleGoBack}
+          className="flex items-center gap-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-arrow-left"
+          >
+            <path d="m12 19-7-7 7-7" />
+            <path d="M19 12H5" />
+          </svg>
+          {language === "id" ? "Kembali" : "Back1"}
+        </Button>
+      </div>*/}
       <div className="mb-8 flex flex-col md:flex-row md:items-center md:justify-between">
         <div>
           <h1 className="text-3xl font-bold text-gray-900">
