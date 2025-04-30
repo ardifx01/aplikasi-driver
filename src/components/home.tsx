89cb39ed93a0592526378f14c538d0efd80bf1b6
Travelintrips <divatranssoetta@gmail.com>
2025-04-30 12:38:14 +0700

    Update home.tsx
    ubah tampilan hp

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
